cb2f33d81c26ad1881016e77590859a36b0f0cd1
Gideon Oba <deyonoba@gmail.com>
2024-09-04 21:50:05 +0100

    update index.js routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UserController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
@@ -8,5 +11,20 @@ router.get('/status', AppController.getStatus);
 
 // GET /stats => AppController.getStats
 router.get('/stats', AppController.getStats);
+
+// POST /users => AppController.postNew
+router.post('/users', AppController.postNew);
+
+// POST /user => UsersController.postNew
+router.get('/users/me', UserController.getMe);
+
+// router.get('/connect', AuthController.getConnect);
+// router.get('/disconnect', AuthController.getDisconnect);
+// router.post('/files', FilesController.postUpload);
+// router.get('/files/:id', FilesController.getShow);
+// router.get('/files', FilesController.getIndex);
+// router.put('/files/:id/publish', FilesController.putPublish);
+// router.put('/files/:id/unpublish', FilesController.putUnpublish);
+// router.get('/files/:id/data', FilesController.getFile);
 
 export default router;
